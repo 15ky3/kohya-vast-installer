93a2421b1ebd9c36c03c77845adbae4fa2aabab8
15ky3 <88146031+15ky3@users.noreply.github.com>
2023-05-16 22:17:40 +0200

    Update vast2.sh

--- vast2.sh
+++ vast2.sh
@@ -23,6 +23,27 @@ conda activate kohya
 conda install pytorch==1.13.1 torchvision==0.14.1 xformers -c pytorch -c nvidia -c xformers
 conda install -c conda-forge cudatoolkit=11.8.0
 python3 -m pip install 'nvidia-cudnn-cu11>=8.6<9' tensorflow==2.11.* tensorrt==8.6.1 triton
+python3 -m pip install accelerate==0.18.0
+python3 -m pip install albumentations==1.3.0
+python3 -m pip install altair==4.2.2
+python3 -m pip install dadaptation==1.5
+python3 -m pip install diffusers[torch]==0.10.2
+python3 -m pip install easygui==0.98.3
+python3 -m pip install einops==0.6.0
+python3 -m pip install ftfy==6.1.1
+python3 -m pip install gradio==3.28.1
+python3 -m pip install lion-pytorch==0.0.6
+python3 -m pip install opencv-python==4.7.0.68
+python3 -m pip install pytorch-lightning==1.9.0
+python3 -m pip install safetensors==0.2.6
+python3 -m pip install toml==0.10.2
+python3 -m pip install voluptuous==0.13.1
+python3 -m pip install wandb==0.15.0
+python3 -m pip install fairscale==0.4.13
+python3 -m pip install requests==2.28.2
+python3 -m pip install timm==0.6.12
+python3 -m pip install huggingface-hub==0.13.3
+python3 -m pip install lycoris_lora==0.1.4
 mkdir -p $CONDA_PREFIX/etc/conda/activate.d
 echo 'CUDNN_PATH=$(dirname $(python -c "import nvidia.cudnn;print(nvidia.cudnn.__file__)"))' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
 echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/:$CUDNN_PATH/lib' >> $CONDA_PREFIX/etc/conda/activate.d/env_vars.sh
